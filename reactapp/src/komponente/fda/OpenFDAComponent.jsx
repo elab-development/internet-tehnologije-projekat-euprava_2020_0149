e827c2a93d8c159ef9acc22303c61e812c2e924e
Ristic2001 <mata.ristic@gmail.com>
2024-01-31 22:16:38 +0100

    dodata biblioteka za paginaciju

--- reactapp/src/komponente/fda/OpenFDAComponent.jsx
+++ reactapp/src/komponente/fda/OpenFDAComponent.jsx
@@ -1,17 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import FDAKartica from './FDAKartica';
-
+import ReactPaginate from 'react-paginate';
 const OpenFDAComponent = () => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
 
+  const [currentPage, setCurrentPage] = useState(0);
+  const itemsPerPage = 5;
+  const pageCount = Math.ceil(data.length / itemsPerPage);
+  const handlePageClick = (event) => {
+    setCurrentPage(event.selected);
+  };
+  const currentItems = data.slice(currentPage * itemsPerPage, (currentPage + 1) * itemsPerPage);
   useEffect(() => {
+
+    
     const fetchData = async () => {
       try {
         
-        const result = await axios.get('https://api.fda.gov/food/enforcement.json?limit=5');
+        const result = await axios.get('https://api.fda.gov/food/enforcement.json?limit=50');
         setData(result.data.results);
       } catch (error) {
         setError(error);
@@ -29,7 +38,7 @@ const OpenFDAComponent = () => {
     <div style={styles.container}>
       <h2>OpenFDA Food Recall Data</h2>
       <ul>
-        {data.map((item, index) => (
+        {currentItems.map((item, index) => (
             <FDAKartica
             key={index}
             proizvod={item.product_description}
@@ -39,6 +48,16 @@ const OpenFDAComponent = () => {
         />
         ))}
       </ul>
+      <ReactPaginate
+            previousLabel={"Prethodna"}
+            nextLabel={"Sledeća"}
+            pageCount={pageCount}
+            onPageChange={handlePageClick}
+            containerClassName={"pagination"}
+            activeClassName={"active"}
+            pageLinkClassName={"pageItem"}
+            />
+
     </div>
   );
 };
@@ -51,6 +70,31 @@ const styles = {
       height: 'auto',
       background: 'linear-gradient(135deg, #e2dadb 25%, #dae2df 25%, #dae2df 50%, #a2a7a5 50%, #a2a7a5 75%, #6d696a 75%)',
       padding: '20px 0'
-    }
+    },
+    pagination: {
+        listStyle: 'none',
+        display: 'flex',
+        justifyContent: 'center',
+        padding: '0',
+      },
+      pageItem: {
+        margin: '0 5px',
+        border: '1px solid #dae2df',
+        borderRadius: '5px',
+        padding: '5px 10px',
+        cursor: 'pointer',
+        backgroundColor: '#f9f9f9',
+        '&:hover': {
+          backgroundColor: '#e2dadb',
+        },
+      },
+      active: {
+        backgroundColor: '#dae2df',
+        color: '#6d696a',
+        '&:hover': {
+          backgroundColor: '#dae2df',
+        },
+      },
+
 };
 export default OpenFDAComponent;
